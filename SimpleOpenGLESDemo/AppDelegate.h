d98f9ee13a1a605b516b70e0cd4ff613d952a13c
arty <vyacheslav.artemev@gmail.com>
2012-07-01 12:31:39 +0400

    Initial Commit

---
+++ SimpleOpenGLESDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SimpleOpenGLESDemo
+//
+//  Created by Arty on 7/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
